f1e43af4824b2c3943b193f462c1a2506942a7a9
Dániel Gulyás <dani@jazzware.io>
2024-05-18 08:42:34 +0200

    Vue basics.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
-
 const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <main class="mx-auto max-w-md w-full mt-24">
+    <header class="text-2xl">Mega counter is <span id="mega-counter">{{count}}</span></header>
+    <button class="bg-gray-200 py-2.5 px-3 rounded-md mt-4" @click="count += 1">turn up</button>
+  </main>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
+<style>
 </style>
